f5a632565a782e1c2487ab5dac0d05c446363319
cruztadeoxid <128651375+cruztadeoxid@users.noreply.github.com>
2023-09-05 22:37:19 -0500

    [add] client websocket

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+import { io } from "socket.io-client";
+
+const socket = io('http://localhost:3001')
+
+socket.on("connect", () => {
+  console.log(socket.id, 'probando'); // x8WIv7-mJelg7on_ALbx
+});
+
+socket.emit('message', 'Hola mundo probadno')
+
+</script>
 <template>
   <main>
     <h2>Probando el sistem completo</h2>
